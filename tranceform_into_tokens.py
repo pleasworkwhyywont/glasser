bfeef188d7660e1eb5fd359d9604cef00e020089
pleasworkwhyywont <micahp11585@mnpsk12.org>
2025-09-05 18:42:47 +0000

    tokenizer better

--- tranceform_into_tokens.py
+++ tranceform_into_tokens.py
@@ -8,20 +8,26 @@ def is_any_element_of_a_list_is_in_str(List,str):
 def tonken_converter(text:str):
     tokens = []
     token = ""
+
     for i in text:
+        token += i
         if token in key_words:
             tokens.append((Tokens.KEY_WORDS,token))
             token = ""
         print(i in key_symbols)
         print("\""+i+"\"")
         if i in key_symbols:
-            tokens.append((Tokens.TEXT,token))
             tokens.append((Tokens.KEY_SYMBOLS,i))
+            if not token in key_symbols:
+                print
+                token = token[1,token.len-1]
+                tokens.append((Tokens.TEXT,token))
             token = ""
         if token in Basic_types:
             tokens.append((Tokens.KEY_TYPES,token))
             token = ""
-        token += i
         print(token)
     return tokens
-print(tonken_converter("def var test | int = 1 life_time = 1 |"))
+a = tonken_converter("def var test | int = 1 life_time = 1 |")
+for i in a:
+    print(i)
